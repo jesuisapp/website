0a14a1bcd43bf0cd1ab0e7d63699b9e570422484
Alexander Liu <accounts@alexanderliu.com>
2022-05-26 00:15:28 +0000

    feat: add qr code redirect for antrepreneur pitch one

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        // ANTrepreneur Pitch 1 (QR)
+        source: "/links/pitch-QM53c",
+        destination: "https://twitter.com/jesuisapp",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
